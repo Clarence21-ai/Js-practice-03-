f283b13a5658d902f44a3cfaf7cdb3c20a39bb9c
Clarence21-ai <agtualclarence16@gmail.com>
2024-09-15 02:20:04 +0000

    initial commit with app.js

---
+++ dataConversion.js
@@ -0,0 +1,23 @@
+let price = "200";
+
+let priceAsInt = parseInt(price);
+console.log("Using parseInt():", priceAsInt); 
+
+let priceAsNumber = Number(price);
+console.log("Using Number():", priceAsNumber);
+
+let priceWithPlus = +price;
+console.log("Using + operator:"
+
+let disntance = 150;
+
+let distanceAsString1 = distance.toString();
+console.log("Using toString():", distanceAsString1); 
+
+let distanceAsString2 = String(distance);
+console.log("Using String():", distanceAsString2); 
+
+let weight = "72.5";
+
+let weightAsDecimal = parseFloat(weight);
+console.log("Using parseFloat():", weightAsDecimal); 
